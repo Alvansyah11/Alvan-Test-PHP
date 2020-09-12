4620d1d7edd522530780a74c7a1ee4894edcf164
M Ardiansah <alvanwongjowo@gmail.com>
2020-09-12 12:53:04 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <body>
+                <style>
+                    h1,
+                    h2,
+                    p,
+                    a {
+                        font-family: sans-serif;
+                        font-weight: normal;
+                    }
+                    <h2>Hello</h2>
+
+                    .jam-digital-Alvan {
+                        overflow: hidden;
+                        width: 330px;
+                        margin: 20px auto;
+                        border: 5px solid #efefef;
+                    }
+
+                    .kotak {
+                        float: left;
+                        width: 110px;
+                        height: 100px;
+                        background-color: #189fff;
+                    }
+
+                    .jam-digital-Alvan p {
+                        color: #fff;
+                        font-size: 36px;
+                        text-align: center;
+                        margin-top: 30px;
+                    }
+                </style>
+
+                <div class="jam-digital-Alvan">
+                    <div class="kotak">
+                        <p id="jam"></p>
+                    </div>
+                    <div class="kotak">
+                        <p id="menit"></p>
+                    </div>
+                    <div class="kotak">
+                        <p id="detik"></p>
+                    </div>
+                </div>
+
+
+                <script>
+                    window.setTimeout("waktu()", 1000);
+
+                    function waktu() {
+                        var waktu = new Date();
+                        setTimeout("waktu()", 1000);
+                        document.getElementById("jam").innerHTML = waktu.getHours();
+                        document.getElementById("menit").innerHTML = waktu.getMinutes();
+                        document.getElementById("detik").innerHTML = waktu.getSeconds();
+                    }
+                </script>
+            </body>
+        </div>
+    </body>
+</html>
